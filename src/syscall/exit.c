46691fd2d40aff91114e562cb89ad02f164cfe3a
Michal Bednarski <bednarski.michal2@gmail.com>
2024-04-14 11:53:21 +0200

    Fix forcing result after PR_void on AArch32
    termux/proot-distro#404

--- src/syscall/exit.c
+++ src/syscall/exit.c
@@ -74,6 +74,19 @@ void translate_syscall_exit(Tracee *tracee)
 		goto end;
 	}
 
+	/* If proot changed syscall to PR_void during enter,
+	 * keep syscall result set during entry. */
+	if (peek_reg(tracee, MODIFIED, SYSARG_NUM) ==
+#if defined(ARCH_ARM64) || defined(ARCH_X86_64)
+			(is_32on64_mode(tracee) ? (SYSCALL_AVOIDER & 0xFFFFFFFF) : SYSCALL_AVOIDER)
+#else
+			SYSCALL_AVOIDER
+#endif
+			&&
+			peek_reg(tracee, ORIGINAL, SYSARG_NUM) != peek_reg(tracee, MODIFIED, SYSARG_NUM)) {
+		poke_reg(tracee, SYSARG_RESULT, peek_reg(tracee, MODIFIED, SYSARG_RESULT));
+	}
+
 	/* Translate output arguments:
 	 * - break: update the syscall result register with "status"
 	 * - goto end: nothing else to do.
