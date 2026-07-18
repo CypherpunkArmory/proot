6e0c12bdcafdbf583a650b5542844bd461b353da
Sylirre <sylirre@termux.dev>
2026-07-18 21:50:48 +0000

    canon: take alternate approach addressing deep symlinks recursion
    Inputs:
    
    * https://github.com/termux/proot-distro/issues/567#issuecomment-4471667885
    * https://github.com/termux/proot/issues/382

--- src/path/canon.c
+++ src/path/canon.c
@@ -206,7 +206,8 @@ int canonicalize(Tracee *tracee, const char *user_path, bool deref_final,
 	unsigned int symlinks_followed = 0;
 
 	/* Avoid infinite loop on circular links.  */
-	if (recursion_level > MAXSYMLINKS)
+	//if (recursion_level > MAXSYMLINKS)
+	if (recursion_level > 3)
 		return -ELOOP;
 
 	/* Sanity checks.  */
